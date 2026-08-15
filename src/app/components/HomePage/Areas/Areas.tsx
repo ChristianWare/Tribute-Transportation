649b3b450c068727ad912d724632fc3cd184dad4
Chris Ware <c.p.ware@hotmail.com>
2026-08-14 21:51:41 -0700

    Areas start

---
+++ src/app/components/HomePage/Areas/Areas.tsx
@@ -0,0 +1,14 @@
+import LayoutWrapper from "../../shared/LayoutWrapper";
+import styles from "./Areas.module.css";
+
+export default function Areas() {
+  return (
+    <section className={styles.container}>
+        <LayoutWrapper>
+            <div className={styles.content}>
+                
+            </div>
+        </LayoutWrapper>
+    </section>
+  )
+}
