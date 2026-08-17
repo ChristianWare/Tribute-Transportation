11a9b19ae06f405f22e72af6a212c454f0d1331b
Chris Ware <c.p.ware@hotmail.com>
2026-08-17 09:38:45 -0700

    About hero started

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import AboutUsHero from "@/components/AboutUsPage/AboutUsHero/AboutUsHero";
+import Nav from "@/components/shared/Nav/Nav";
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <main>
+      <Nav />
+      <AboutUsHero />
+    </main>
+  );
+}
